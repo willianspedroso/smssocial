4a10a13ffee4dbd96905d74ab266b2c134a25e91
willianspedroso <willianspedroso@gmail.com>
2016-01-11 11:53:44 -0200

    Atualização do CSS

--- ajuda.php
+++ ajuda.php
@@ -1,18 +1,38 @@
-<div >
-  <div >
-    <span>
-      <b>SMS</b> Social
-      <img src="<?php bloginfo('stylesheet_directory'); ?>/imagens/smsSocial.png" width="50" title="SMSSocial">
-    </span>
-  </div>
-  <br>
-  <br>
-  <div class="row">
-      <div class="col-xs-12">
-        <h1><a href="#" >Perguntas Frequentes</a></h1>
-      </div>
-      <div class="col-xs-12">
-        <h1><a href="#" >Manual do Usuário</a></h1>
-      </div>
+<div class="login-box" style="width:720px;">
+  <div class="login-logo">
+    <div id="blog-title">
+      <span>
+        <!--<b>SMS</b> Social-->
+        <img src="<?php bloginfo('stylesheet_directory'); ?>/imagens/smsSocial.png" width="160" title="SMSSocial">
+      </span>
     </div>
-</div>
+  </div><!-- /.login-logo -->
+  <div class="login-box-body">
+    <section class="content-header">
+      <h1>Ajuda</h1>
+    </section>
+    <div class="col-xs-12">&nbsp;</div>
+    <div class="row">
+      <div class="col-xs-6">
+          <div class="box">
+            <div class="box-body">
+              <center>
+              <a style="font-size:45pt" href="#"><i class="fa fa-play-circle-o"></i></a><br>
+              <span style="font-size:10pt">Vídeo Tutorial</span>
+              </center>
+            </div>
+          </div>
+      </div>
+      <div class="row">
+      <div class="col-xs-6">
+          <div class="box">
+            <div class="box-body">
+              <center>
+              <a style="font-size:45pt" href="#"><i class="fa fa-book"></i></a><br>
+              <span style="font-size:10pt">Manual do Usuário</span>
+              </center>
+            </div>
+          </div>
+      </div>
+  </div><!-- /.login-box-body -->
+</div><!-- /.fale-box -->
--- fale_conosco.php
+++ fale_conosco.php
@@ -1,64 +1,45 @@
- <?php
-//pega os dados do usuario que está logado
-$current_user = wp_get_current_user();
-if($current_user->ID == 0) {
-?>
-	<div >
-	  <div >
-	    <span>
-	      <b>SMS</b> Social
-	      <img src="<?php bloginfo('stylesheet_directory'); ?>/imagens/smsSocial.png" width="50" title="SMSSocial">
-	    </span>
-	  </div>
-	</div>
-	<br>
-	<br>
-<?php
-}
-?>
+<div class="login-box" style="width:720px;">
+  <div class="login-logo">
+    <div id="blog-title">
+      <span>
+        <!--<b>SMS</b> Social-->
+        <img src="<?php bloginfo('stylesheet_directory'); ?>/imagens/smsSocial.png" width="160" title="SMSSocial">
+      </span>
+    </div>
+  </div><!-- /.login-logo -->
+  <div class="login-box-body">
+    <section class="content-header">
+      <h1>Fale Conosco</h1>
+    </section>
+    <div class="col-xs-12">&nbsp;</div>
+    <form name="form" id="form" action="<?php bloginfo('template_url'); ?>/login/actions.php?mt=fale_conosco" method="post" >
+      <div class="col-xs-12">
+        <label> Nome </label>
+        <input type="text" name="nome" value="" placeholder="Nome" class="form-control">
+      </div>
+      <div class="col-xs-12">&nbsp;</div>
 
-<!-- Content Header (Page header) -->
-<section class="content-header">
-  <h1>Fale Conosco</h1>
-</section>
+      <div class="col-xs-6">
+        <label> Email </label>
+        <input type="text" name="email" value="" placeholder="Email" class="form-control">
+      </div>
 
-<!-- Main content -->
-<section class="content">
-	<div class="row">
-		<div class="col-xs-12">
-			<div class="box">
-				<div class="box-header">          
-					<form name="form" id="form" action="<?php bloginfo('template_url'); ?>/login/actions.php?mt=fale_conosco" method="post" >
-						<div class="col-md-6">
-							<label> Nome </label>
-							<input type="text" name="nome" value="" placeholder="Nome" class="form-control">
-						</div>
-						<div class="col-md-12">&nbsp;</div>
+      <div class="col-xs-6">
+        <label> Telefone </label>
+        <input type="text" name="telefone" value="" placeholder="Telefone" class="form-control">
+      </div>
+      <div class="col-xs-12">&nbsp;</div>
 
-						<div class="col-md-6">
-							<label> Email </label>
-							<input type="text" name="email" value="" placeholder="Email" class="form-control">
-						</div>
-						<div class="col-md-12">&nbsp;</div>
-
-						<div class="col-md-6">
-							<label> Telefone </label>
-							<input type="text" name="telefone" value="" placeholder="Telefone" class="form-control">
-						</div>
-						<div class="col-md-12">&nbsp;</div>
-
-						<div class="col-md-6">
-							<label> Texto </label>
-							<textarea rows="3" cols="50" name="texto" class="form-control"></textarea>
-						</div>
-						<div class="col-md-12">&nbsp;</div>
-
-						<div class="col-md-12">
-							<button class="btn  btn-primary" type="submit">Enviar</button>
-						</div>
-					</form>
-       			</div><!-- /.box-header -->
-			</div>
-		</div>
-	</div>
-</section>
+      <div class="col-xs-12">
+        <label> Texto </label>
+        <textarea rows="2" cols="50" name="texto" class="form-control"></textarea>
+      </div>
+      <div class="col-xs-12">&nbsp;</div>
+      <div class="row">
+        <div class="col-xs-12">
+          <button class="btn  btn-primary" type="submit">Enviar</button>
+        </div>
+      </div>
+    </form>
+  </div><!-- /.login-box-body -->
+</div><!-- /.fale-box -->
